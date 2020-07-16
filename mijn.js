a89532b78ebe4e8f9ff18eed084d703195f0a903
evva <evgeniia.proskuriakova@hotmail.com>
2020-07-16 14:30:51 +0200

    Initial commit

---
+++ mijn.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+    $('.parallax-window').parallax({
+        imageSrc: '../img/1.jpg'
+    });
+    $('.parallax-window-two').parallax({
+        imageSrc: '../img/w.jpg'
+    });
+});
+
+
+/*mouseparallax*/
+$("#container").mousemove(function(e) {
+    parallaxIt(e, ".slide", -100);
+    parallaxIt(e, "img", -30);
+});
+
+function parallaxIt(e, target, movement) {
+    var $this = $("#container");
+    var relX = e.pageX - $this.offset().left;
+    var relY = e.pageY - $this.offset().top;
+
+    TweenMax.to(target, 1, {
+        x: (relX - $this.width() / 2) / $this.width() * movement,
+        y: (relY - $this.height() / 2) / $this.height() * movement
+    });
+}
+
+
+
+
+/*Panelen*/
+$(window).scroll(function() {
+
+    // selectors
+    var $window = $(window),
+        $body = $('body'),
+        $panel = $('.panel');
+
+    // Change 33% earlier than scroll position so colour is there when you arrive.
+    var scroll = $window.scrollTop() + ($window.height() / 3);
+
+    $panel.each(function () {
+        var $this = $(this);
+
+        // if position is within range of this panel.
+        // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
+        // Remember we set the scroll to 33% earlier in scroll var.
+        if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+
+            // Remove all classes on body with color-
+            $body.removeClass(function (index, css) {
+                return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
+            });
+
+            // Add class of currently active div
+            $body.addClass('color-' + $(this).data('color'));
+        }
+    });
+
+}).scroll();
+
+
